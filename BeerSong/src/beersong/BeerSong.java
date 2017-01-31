ced0d0ef1cf3ee6478b84a609126001251267eba
Matt Ushman <matthew.ushman@gmail.com>
2017-01-30 19:41:58 -0500

    Added logic for Main() and Ninety_Nine_Bottles_of_Beer(), program works as intended.

--- BeerSong/src/beersong/BeerSong.java
+++ BeerSong/src/beersong/BeerSong.java
@@ -5,19 +5,31 @@
  */
 package beersong;
 
+
+
 /**
  *
  * @author Matt
  */
 public class BeerSong {
+
     /**
      * @param args the command line arguments
      */
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         
+        new BeerSong().Ninety_Nine_bottles_of_Beer();
     }
     
     public void Ninety_Nine_bottles_of_Beer(){
         
-    }   
+        int i = 99;
+        
+        while(i>1){
+            System.out.println(i + " bottels of beer on the wall, " + i + " bottels of beer take one down, pass it around, ");
+            i--;
+            System.out.println(i + " bottels of beer on the wall.");
+        }
+        System.out.println("1 bottel of beer on the wall, 1 bottel of beer take one down, pass it around, no more bottels of beer on the wall.");
+    }
 }
